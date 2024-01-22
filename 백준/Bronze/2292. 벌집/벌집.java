b3bc6b56e3a48f80643fc28459ec823926bd1e18
newbe93 <tlsehd930@gmail.com>
2024-01-22 19:10:07 +0900

    [Bronze II] Title: 벌집, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int count = 1;
+		while(N > 1) {
+			N = N- count*6;
+			count++;
+		}
+		System.out.println(count);
+		
+	}
+
+}
+
+/*
+
+시작방 1번		1번
+2층은 1+ 6 = 7 번까지
+3층은 7+ 12		19번
+4층	19 + 18		37번
+		24		61번
+		=> 층계가 1층씩 커질때마다 6의 배수씩 커지는걸 볼수있다.
+*/
