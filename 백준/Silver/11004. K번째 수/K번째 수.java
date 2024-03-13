7d5d6d4c00f7c4e6082362d835f96dd8ddc03f12
newbe93 <tlsehd930@gmail.com>
2024-03-13 21:17:57 +0900

    [Silver V] Title: K번째 수, Time: 1132 ms, Memory: 368812 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,70 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+
+        for(int i=0 ; i<N ; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        quickSort(A,0,N-1,K-1);
+        System.out.println(A[K-1]);
+    }
+
+    private static void quickSort(int[] A, int S, int E, int K) {
+        if(S < E){
+            int pivot = partition(A,S,E);
+            if(pivot == K) return;
+            else if (pivot > K) {
+                quickSort(A,S,pivot-1,K);
+            } else {
+                quickSort(A,pivot+1,E,K);
+            }
+        }
+    }
+
+
+    public static int partition(int[] A, int S, int E){
+        if(S+1 == E){
+            if(A[S] > A[E]) swap(A,S,E);
+        }
+        int M = (S+E)/2;
+        swap(A,S,M);
+        int pivot = A[S];
+        int i = S+1;
+        int j = E;
+        while(i <= j){
+            while(i<=E && pivot > A[i]){
+                i++;
+            }
+            while(j>=S+1 && pivot < A[j]){
+                j--;
+            }
+            if(i <= j){
+                swap(A,i++,j--);
+            }
+        }
+        A[S] = A[j];
+        A[j] = pivot;
+        return j;
+    }
+
+    private static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
+
+}
